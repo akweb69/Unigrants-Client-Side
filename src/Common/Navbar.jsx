b3d4541a8fcf9786a82998ef7cb305e287369f00
Abu Kalam <akwebdev69@gmail.com>
2025-01-14 14:58:39 +0600

    design the initial navbar

--- src/Common/Navbar.jsx
+++ src/Common/Navbar.jsx
@@ -1,8 +1,38 @@
+import { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
+    const [hover, setHover] = useState(false)
     return (
-        <div>
-            Navbar
+        <div className="w-full h-16 backdrop-blur-md">
+            <div className="w-11/12 border border-red-500 mx-auto h-full flex justify-between items-center">
+                {/* left logo */}
+                <div className="">
+                    <h1 className="text-2xl md:text-4xl font-logoFont font-extrabold"><b className="text-orange-500">U</b>ni<b className="text-orange-500">G</b>rants</h1>
+                </div>
+                {/* right links */}
+                <div className="flex-1">
+                    <div className="flex w-full items-center justify-end lg:text-lg font-secondary">
+
+                        <NavLink className='hover:bg-orange-400 hover:text-white rounded-sm px-[0.75rem] py-[0.5rem]' to={'/'}>Home</NavLink>
+
+                        <NavLink className='hover:bg-orange-400 hover:text-white rounded-sm px-[0.75rem] py-[0.5rem]' to={'/all-scholarship'}>All Scholarship</NavLink>
+
+                        <NavLink className='hover:bg-orange-400 hover:text-white rounded-sm px-[0.75rem] py-[0.5rem]' to={'/user-dashboard'}>Dashboard</NavLink>
+
+                        <div className="w-12 h-12 ml-4 rounded-full border border-orange-500 ">
+                            <img className="w-full h-full rounded-full" src="" alt="" />
+                        </div>
+                        <div className="">
+                            <Link
+                                onMouseEnter={() => setHover(true)}
+                                onMouseLeave={() => setHover(false)}
+                                to={'/login'} className="px-3 py-2 rounded-sm hover:bg-orange-500 border border-orange-500 hover:text-white">Login</Link>
+                            <Link to={"/register"} className={`px-3 py-2 rounded-sm  border border-orange-500 text-white ${hover ? "bg-white text-black" : "bg-orange-500"}`}>Register</Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
