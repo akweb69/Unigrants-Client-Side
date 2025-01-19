2a3c2d8a431e9bad80747e163d02271ec81d9ed3
Abu Kalam <akwebdev69@gmail.com>
2025-01-19 12:16:43 +0600

    Implement the Moderator Rout

--- src/PrivateRout/ModeratorRout.jsx
+++ src/PrivateRout/ModeratorRout.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../AuthContext/AuthProvider";
+import { Navigate } from "react-router-dom";
+import useAllUser from "../Hooks/useAllUser";
 
-const ModeratorRout = () => {
-    return (
-        <div>
+const ModeratorRout = ({ children }) => {
 
-        </div>
-    );
+    const [role, setRole] = useState("");
+    const [users, refetch, isLoading] = useAllUser();
+    const { user, loading, logout } = useContext(AuthContext);
+
+    useEffect(() => {
+        const dUser = users.filter(hi => hi.email === user?.email)[0]
+        setRole(dUser?.role)
+    }, [users, isLoading, user])
+
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+
+    if (!user && !user?.email) {
+        return <Navigate to="/login" replace />;
+    }
+    if (role !== "Moderator") {
+        return <Navigate to="/login" replace />;
+    }
+
+    if (role === "Moderator") {
+        return children;
+    }
 };
 
 export default ModeratorRout;
