9c85286af94ef0e536551c632a8540fbaa1e2e25
Abu Kalam <akwebdev69@gmail.com>
2025-01-19 12:19:48 +0600

    Implement the Admin rout

--- src/PrivateRout/AdminRout.jsx
+++ src/PrivateRout/AdminRout.jsx
@@ -1,11 +1,35 @@
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../AuthContext/AuthProvider";
+import { Navigate } from "react-router-dom";
+import useAllUser from "../Hooks/useAllUser";
 
-const AdminRout = () => {
+const AdminRout = ({ children }) => {
 
-    return (
-        <div>
+    const [role, setRole] = useState("");
+    const [users, refetch, isLoading] = useAllUser();
+    const { user, loading, logout } = useContext(AuthContext);
 
-        </div>
-    );
+    useEffect(() => {
+        const dUser = users.filter(hi => hi.email === user?.email)[0]
+        setRole(dUser?.role)
+    }, [users, isLoading, user])
+
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+
+    if (!user && !user?.email) {
+        return <Navigate to="/login" replace />;
+    }
+    if (role !== "Admin") {
+        return <Navigate to="/login" replace />;
+    }
+
+    if (role === "Admin") {
+        return children;
+    }
 };
 
 export default AdminRout;
