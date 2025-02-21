69d1c5c479629c1c382e6941960d4f4c1ae14257
Abu Kalam <akwebdev69@gmail.com>
2025-02-21 07:27:13 +0600

    updated the banner design

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -3,6 +3,7 @@ import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import { Link, useNavigate } from "react-router-dom";
 import { FaSearch } from "react-icons/fa";
+import { motion } from "framer-motion";
 const Banner = () => {
     const settings = {
         infinite: true,
@@ -24,12 +25,24 @@ const Banner = () => {
                     <Slider {...settings}>
                         <div className="">
                             <div className="w-full h-[80vh] flex justify-center flex-col bg-slider-01 items-center bg-center text-center bg-cover bg-no-repeat bg-[rgba(0,0,0,0.6)] bg-blend-overlay ">
-                                <h1 className="text-4xl px-4 md:text-6xl lg:text-8xl text-transparent bg-gradient-to-tr from-orange-500 to-fuchsia-500 bg-clip-text w-fit mx-auto  font-extrabold font-logoFont py-4">Find Your Dream Scholarship</h1>
+                                <motion.h1
+                                    initial={{ y: 80, opacity: 0 }}
+                                    animate={{ y: 0, opacity: 1 }}
+                                    transition={{ duration: 0.6, delay: 0.3 }}
+                                    className="text-4xl px-4 md:text-6xl lg:text-8xl text-transparent bg-gradient-to-tr from-orange-500 to-fuchsia-500 bg-clip-text w-fit mx-auto  font-extrabold font-logoFont py-4 animate-bounce">Find Your Dream Scholarship</motion.h1>
 
-                                <p className="md:text-xl font-semibold  text-transparent bg-gradient-to-l from-orange-500 to-fuchsia-500 bg-clip-text w-fit mx-auto  px-4">Explore scholarships that match your academic goals.</p>
+                                <motion.p
+                                    initial={{ y: 130, opacity: 0 }}
+                                    animate={{ y: 0, opacity: 1 }}
+                                    transition={{ duration: 0.6, delay: 0.9 }}
+                                    className="md:text-xl font-semibold  text-transparent bg-gradient-to-l from-orange-500 to-fuchsia-500 bg-clip-text w-fit mx-auto  px-4">Explore scholarships that match your academic goals.</motion.p>
 
                                 <div className="py-7 w-full">
-                                    <div className="w-11/12 md:w-1/3 mx-auto p-1 rounded-full border border-orange-500 mb-10">
+                                    <motion.div
+                                        initial={{ y: 130, opacity: 0 }}
+                                        animate={{ y: 0, opacity: 1 }}
+                                        transition={{ duration: 0.6, delay: 1.5 }}
+                                        className="w-11/12 md:w-1/3 mx-auto p-1 rounded-full border border-orange-500 mb-10">
                                         <div className="w-full flex justify-between items-center gap-1 flex-grow">
                                             <div className="px-2"><FaSearch className="text-orange-500 "></FaSearch> </div>
                                             <div className="flex-1"><input type="text" className="w-full outline-none bg-transparent border-0 text-white" placeholder="Search Scholaship by university , country ..." /></div>
@@ -40,18 +53,30 @@ const Banner = () => {
                                                 Search
                                             </div>
                                         </div>
-                                    </div>
+                                    </motion.div>
                                 </div>
                             </div>
                         </div>
                         <div className="">
                             <div className="w-full h-[80vh] flex justify-center flex-col bg-slider-02 items-center bg-center text-center bg-cover bg-no-repeat bg-[rgba(0,0,0,0.6)] bg-blend-overlay ">
-                                <h1 className="text-4xl px-4 md:text-6xl lg:text-8xl text-transparent bg-gradient-to-tr from-orange-500 to-fuchsia-500 bg-clip-text w-fit mx-auto  font-extrabold font-logoFont py-4">Achieve Excellence</h1>
+                                <motion.h1
+                                    initial={{ y: 0, opacity: 0 }}
+                                    animate={{ y: 0, opacity: 1 }}
+                                    transition={{ duration: 1, delay: 6 }}
+                                    className="text-4xl px-4 md:text-6xl lg:text-8xl text-transparent bg-gradient-to-tr from-orange-500 to-fuchsia-500 bg-clip-text w-fit mx-auto  font-extrabold font-logoFont py-4">Achieve Excellence</motion.h1>
 
-                                <p className="md:text-xl font-semibold  text-transparent bg-gradient-to-l from-orange-500 to-fuchsia-500 bg-clip-text w-fit mx-auto  px-4">Turn your aspirations into achievements with the right funding.</p>
+                                <motion.p
+                                    initial={{ y: 80, opacity: 0 }}
+                                    animate={{ y: 0, opacity: 1 }}
+                                    transition={{ duration: 0.6, delay: 6.3 }}
+                                    className="md:text-xl font-semibold  text-transparent bg-gradient-to-l from-orange-500 to-fuchsia-500 bg-clip-text w-fit mx-auto  px-4">Turn your aspirations into achievements with the right funding.</motion.p>
 
                                 <div className="py-7 w-full">
-                                    <div className="w-11/12 md:w-1/3 mx-auto p-1 rounded-full border border-orange-500 mb-10">
+                                    <motion.div
+                                        initial={{ y: 80, opacity: 0 }}
+                                        animate={{ y: 0, opacity: 1 }}
+                                        transition={{ duration: 0.6, delay: 6.77 }}
+                                        className="w-11/12 md:w-1/3 mx-auto p-1 rounded-full border border-orange-500 mb-10">
                                         <div className="w-full flex justify-between items-center gap-1 flex-grow">
                                             <div className="px-2"><FaSearch className="text-orange-500 "></FaSearch> </div>
                                             <div className="flex-1"><input type="text" className="w-full outline-none bg-transparent border-0 text-white" placeholder="Search Scholaship by university , country ..." /></div>
@@ -62,7 +87,7 @@ const Banner = () => {
                                                 Search
                                             </div>
                                         </div>
-                                    </div>
+                                    </motion.div>
                                 </div>
                             </div>
                         </div>
